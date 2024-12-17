6b3d40cc59c81d30c811e082bbf7757fa56fa135
Henrik Levsen Simonsen <henrik.l.simonsen@me.com>
2024-12-17 23:32:28 +0100

    feat: introducing errors for testing pipeline

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
+    it('', () => expect(appController).not.toBeDefined());
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World');
     });
   });
 });
